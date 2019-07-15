6144cd28b54f84582c23d6eccf088e499937eea4
Daniel Juarez <djuarez11@cnm.edu>
2019-07-15 17:12:12 -0600

    added persona

---
+++ public_html/documentation/milestone-1.php
@@ -0,0 +1,50 @@
+<html lang="en">
+	<head>
+		<title>
+			Milestone 1
+		</title>
+	</head>
+	<body>
+		<h1>
+			Milestone 1
+		</h1>
+			<h2>
+				Purpose
+			</h2>
+				<p>
+					I want my PWP to help me stand out from the increasingly numerous
+					Web Development bootcamp graduates and demonstrate my front-end skills.
+				</p>
+			<h2>
+				Audience
+			</h2>
+				<p>
+					The primary audience of this site is prospective employers. I want them
+					to know that I am professional, capable, and able to meet their needs.
+				</p>
+			<h2>
+				Goal
+			</h2>
+				<p>
+					To acquire full-time employment working for a company involved in
+					developing web applications.
+				</p>
+		<br>
+		<h1>
+			Target Audience Persona
+		</h1>
+			<p>
+				Bryan Reynolds is a 45 year-old web development manager at a medium to
+				large-sized company. His company uses older, but still functional IDEs
+				and is focused on fantastic UX for their web apps. Bryan knows exactly
+				what he wants out of his employees, but allows them to work fairly
+				independently. He doesn't have a lot of time to look at applicants'
+				websites so he is looking for someone with good attention to detail and
+				a good sense of design and flow, as well as quick access to relevant
+				information, such as resumes and other work. Bryan wants someone who
+				is passionate about developing high quality web applications because they
+				see the potential that they have.
+			</p>
+		</ul>
+	</body>
+</html>
